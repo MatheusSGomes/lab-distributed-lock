b4f858aa45aa03038eb6b11cb15d3e228fb11525
Matheus Gomes <math.s.gomes@gmail.com>
2025-03-12 21:33:54 -0300

    add init connection mongodb

--- index.js
+++ index.js
@@ -1,16 +1,49 @@
 import { createClient } from "redis";
+import { MongoClient } from 'mongodb'
 
-const client = await createClient()
+const clientRedis = await createClient()
     .on('error', err => console.log('Redis client error', err))
     .connect();
 
-await client.set('contador', 0);
-client.incr('contador');
-client.incr('contador');
-client.incr('contador');
+await clientRedis.set('contador', 0);
+clientRedis.incr('contador');
+clientRedis.incr('contador');
+clientRedis.incr('contador');
 
-const value = await client.get('contador');
+const value = await clientRedis.get('contador');
 
 console.log(value);
 
-await client.disconnect();
+await clientRedis.disconnect();
+
+const usuario = 'root';
+const senha = 'example';
+const host = 'localhost';
+const port = '27017';
+
+const url = `mongodb://${usuario}:${senha}@${host}:${port}`;
+const clientMongo = new MongoClient(url);
+
+async function connect() {
+    try {
+        await clientMongo.connect();
+        console.log('Mongo conectado com sucesso')
+        return clientMongo.db('meu_banco').collection('minha_colecao');
+    } catch (error) {
+        console.error("Erro ao conectar", error);
+    }
+}
+
+async function listar() {
+    const collection = await connect();
+    const registros = await collection.find().toArray();
+    console.log(registros);
+}
+
+listar();
+
+
+/*connect()
+    .then(console.log)
+    .catch(console.error)
+     .finally(() => clientMongo.close()) */;
